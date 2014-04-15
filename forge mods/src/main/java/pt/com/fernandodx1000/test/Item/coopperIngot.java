092368fc868778d544340e50dc853b0fa124df69
gonçalo fernando da silva costa <goncalo.96@live.com.pt>
2014-04-15 23:58:36 +0100

    .

---
+++ forge mods/src/main/java/pt/com/fernandodx1000/test/Item/coopperIngot.java
@@ -0,0 +1,12 @@
+package pt.com.fernandodx1000.test.Item;
+
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
+
+public class coopperIngot extends Item {
+
+	public coopperIngot(int i) {
+		this.setCreativeTab(CreativeTabs.tabMaterials);
+	}
+
+}
---
+++ forge mods/src/main/java/pt/com/fernandodx1000/test/block/copperOre.java
@@ -0,0 +1,18 @@
+package pt.com.fernandodx1000.test.block;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.creativetab.CreativeTabs;
+
+public class copperOre extends Block {
+
+	public copperOre(int i, Material p_i45394_1_) {
+		super(p_i45394_1_);
+		this.setCreativeTab(CreativeTabs.tabBlock);
+		this.setHardness(5F);
+		this.setResistance(5.0f);
+	
+		
+	}
+
+}
