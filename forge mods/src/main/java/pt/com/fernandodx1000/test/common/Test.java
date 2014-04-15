5326075628ccf8f269c043631dc789eeeb8069af
gonçalo fernando da silva costa <goncalo.96@live.com.pt>
2014-04-15 23:47:37 +0100

    .

--- forge mods/src/main/java/pt/com/fernandodx1000/test/common/Test.java
+++ forge mods/src/main/java/pt/com/fernandodx1000/test/common/Test.java
@@ -1,7 +1,12 @@
 package pt.com.fernandodx1000.test.common;
 
+import pt.com.fernandodx1000.test.Item.coopperIngot;
+import pt.com.fernandodx1000.test.block.*;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.oredict.OreDictionary;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.registry.GameRegistry;
@@ -11,14 +16,37 @@ public class Test {
 	@SidedProxy(clientSide = "pt.com.fernandodx1000.test.common.ClientProxy", serverSide = "pt.com.fernandodx1000.test.common.CommonProxy")
 	public static CommonProxy proxy;
 
-	//Blocks
-	//public static Block testBlock = new testBlock(300, Material.rock).setBlockName("testblock");
+	// Blocks
+	public static Block copperOre = new copperOre(300, Material.rock)
+			.setBlockName("copperOre").setBlockTextureName("test:copperore");
+
+	// Items
+	public static Item copperIngot = new coopperIngot(4000).setUnlocalizedName("copperIngot").setTextureName("test:copperingot");
 	
 	public Test() {
+
+		registration();
+		addRecipes();
+
+		//ore Dictionary
+		OreDictionary.registerOre("oreCopper", copperOre);
+		OreDictionary.registerOre("ingotCopper", copperIngot);
+	}
+
+	public static void registration() {
 		
+		GameRegistry.registerBlock(copperOre, "copperOre");
 		
-		
+		GameRegistry.registerItem(copperIngot, "copperIngot");
 		
 	}
 
+	public static void addRecipes() {
+		
+		//crafting
+		
+		//smelting
+		GameRegistry.addSmelting(Test.copperOre, new ItemStack(Test.copperIngot), 0.2f);
+		
+	}
 }
